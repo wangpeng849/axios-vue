864916d08263a65345b5187b906cf54cfa56b237
wangpeng849 <849183239@qq.com>
2019-12-31 16:49:53 +0800

    CRUD

--- src/views/3-5.vue
+++ src/views/3-5.vue
@@ -3,35 +3,27 @@
 </template>
 
 <script>
-// 并发请求  同时行进多个请求 并同意处理返回值
+//取消请求  取消正在进行的http请求（了解）
 import axios from 'axios'
 
 export default {
-    //拦截器 ： 在请求或响应处理前拦截
-      //请求拦截器
-      //响应拦截器
-  name: 'axios3-3',
-  created ()  { 
-       //请求拦截器
-       axios.interceptors.request.use(config => {
-           //在发送请求前 做些什么
-           return config
-       },err => {
-           //在请求后 做些什么
-           return Promise.reject(err)
-       })
-        //响应拦截器
-        axios.interceptors.response.use(res => {
-            //请求成功对响应数据 做处理
-            return res
-        },err => {
-            //响应错误 做些什么
-            return Promise.reject(err)
-        })
-        axios.get().then(res => {
-            console.log(res);
-            
-        })
+  name: 'axios3-5',
+  created ()  {
+     let source = axios.CancelToken.source()
+     axios.get('/data.json',{
+         cancelToken:source.token
+     }).then(res=>{
+         console.log(res);
+         
+     }).catch(err => {
+         console.log(err);
+         
+     })
+
+     //取消请求 cancel(message) --> message 可选
+     source.cancel('cancle http ')
+
+     //大多数情况下用不到
   }
  
 }
---
+++ src/views/ContactList.vue
@@ -0,0 +1,142 @@
+<template>
+    <div class="home">
+        <!-- 联系人列表 -->
+        <!-- <van-popup v-model="showList" position="bottom"> -->
+        <van-contact-list
+            v-model="chosenContactId"
+            :list="list"
+            @add="onAdd"
+            @edit="onEdit"
+            @select="onSelect"
+        />
+        <!-- </van-popup> -->
+          <!-- 联系人编辑 -->
+        <van-popup v-model="showEdit" position="bottom">
+        <van-contact-edit
+            :contact-info="editingContact"
+            :is-edit="isEdit"
+            @save="onSave"
+            @delete="onDelete"
+        />
+       </van-popup>
+    </div>
+</template>
+
+<script>
+// import {Button} from 'vant'
+import Vue from 'vue'
+import {ContactList,ContactEdit,Toast} from 'vant'
+import {Popup} from 'vant'
+import axios from 'axios'
+
+Vue
+    .use(ContactList)
+    .use(Popup)
+    .use(Toast)
+    .use(ContactEdit)
+
+export default {
+  name: 'contactList',
+  compoents: {
+    //   [ContactList.name]:ContactList
+    // [Popup.name]:Popup
+  },
+  computed : {
+      currentContact() {
+      const id = this.chosenContactId;
+      return id !== null ? this.list.filter(item => item.id === id)[0] : {};
+    }
+  },
+  data () {
+      return {
+        //     {
+        //       id:1,
+        //       name:'',
+        //       tel:''
+        //   }
+          chosenContactId: null,
+          list : [] , 
+          instance : null,  //axios实例
+          showEdit : false, //编辑弹窗的显隐
+          editingContact : {},//正在编辑的联系人
+          isEdit : false, //新建或编辑
+          showList: true,
+      }
+  },
+  created ()  {
+      this.instance = axios.create({
+          baseURL: "http://localhost:9000/api",
+          timeout: 1000,
+      })
+     this.getList()
+  },
+  methods : {
+      //获取联系人列表
+      getList () {
+        this.instance.get('/contactList')
+        .then(res => {
+            this.list = res.data.data
+        }).catch(() => {
+            Toast('请求失败，请稍后再试')
+            
+        })
+      },
+      onAdd () {
+        this.showEdit = true
+        this.isEdit = false
+      },
+      onEdit (info) {
+        this.showEdit = true
+        this.isEdit = true
+        this.editingContact = info
+      },
+      onSelect () {
+        this.showList = false;
+      },
+      onSave (info) {
+
+        if(this.isEdit){
+            //编辑保存
+            this.editingContact = info
+            this.instance.put('/contact/edit',info).then(res => {
+                 if(res.data.code === 200){
+                     Toast("编辑成功")
+                    this.showEdit = false
+                    this.getList()
+                 }
+            }).catch(()=>{
+                Toast("请求失败! ")
+            })
+        }else{
+            //新建保存
+            this.instance.post('/contact/new/json',info)
+            .then(res => {
+                if(res.data.code === 200){
+                    Toast("新建成功！")
+                    this.showEdit = false
+                    this.getList()
+                }
+            }).catch(()=>{
+                Toast("新建失败! ")
+            })
+        }
+      },
+      onDelete (info) {
+           this.instance.delete('/contact',{
+               params : {
+                   id : info.id
+               }
+           }).then(res => {
+               if(res.data.code === 200){
+                   Toast("删除成功！")
+                     this.showEdit = false
+                    this.getList()
+               }
+           }).catch(()=>{
+                Toast("请求失败")
+           })
+      }
+  }
+ 
+}
+</script>
